77fe8165e67606360d79243c53936df406280fcf
Haruka Mitsushima <haruka-mitsushima@rakus-partners.co.jp>
2023-01-25 15:20:50 +0900

    ユーザー登録機能の修正

--- src/pages/api/mailConditions.ts
+++ src/pages/api/mailConditions.ts
@@ -6,16 +6,27 @@ export default withIronSessionApiRoute(async function loginRoute(
   req,
   res
 ) {
-  const item = await prisma.user.findMany({
-    where: {
-      mailAddress : req.body.formValues.mailAddress,
-    },
-  });
+  const body = { mailAddress: req.body.formValues.mailAddress };
+  const url = 'http://localhost:3005/api/user/mailConditions'
+  const params = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(body),
+  };
+  const response = await fetch(url, params);
+  console.log(response)
+  const item = await response.json();
 
-  if (item[0] ) {
-    res.json({result: false ,message: 'このメールアドレスはすでに登録済みです'});
+  // const item = await prisma.user.findMany({
+  //   where: {
+  //     mailAddress: req.body.formValues.mailAddress,
+  //   },
+  // });
+
+  if (item) {
+    res.json({ result: false, message: 'このメールアドレスはすでに登録済みです' });
   } else {
-    res.json({result: true});
+    res.json({ result: true });
   }
 },
-ironOptions);
+  ironOptions);
